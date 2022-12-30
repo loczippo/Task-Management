69039bb8417c770d87bd47ac430fc13bbd45e734
loczippo <loc-zippo@vn.apachefootwear.com>
2022-12-30 16:03:35 +0700

    change collection users to user

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -27,4 +27,4 @@ const UserSchema = new mongoose.Schema({
     timestamps: true
 })
 
-export default mongoose.model<IUser & mongoose.Document>('users', UserSchema)
+export default mongoose.model<IUser & mongoose.Document>('user', UserSchema)

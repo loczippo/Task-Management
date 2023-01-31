6d5ea94ac424373684daa4da824ca74267b7dec8
loczippo <loc-zippo@vn.apachefootwear.com>
2023-01-31 10:40:23 +0700

    remove tsoa

--- src/modules/index/index.controler.ts
+++ src/modules/index/index.controler.ts
@@ -1,9 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-import { Get, Route } from 'tsoa';
 
-@Route('index')
 class IndexController {
-  @Get('/')
   index = (req: Request, res: Response, next: NextFunction) => {
     try {
       res.status(200).send('API is running....');
